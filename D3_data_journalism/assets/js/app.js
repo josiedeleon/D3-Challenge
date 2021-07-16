cc167ac0d448857f08635c714c15eb602e5f51a6
J_D_L <jocelyndeleon52@gmail.com>
2021-07-15 21:33:43 -0700

    add updates

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,6 +1,108 @@
 //D3 Dabbler 
-//You need to create a scatter plot between two of the data variables such as Healthcare vs. Poverty or Smokers vs. Age.
+//You need to create a scatter plot between two of the data variables Smokers vs. Age.
 //Using the D3 techniques we taught you in class, create a scatter plot that represents each state with circle elements. You'll code this graphic in the app.js file of your homework directory—make sure you pull in the data from data.csv by using the d3.csv function. Your scatter plot should ultimately appear like the image at the top of this section.
 //Include state abbreviations in the circles.
 //Create and situate your axes and labels to the left and bottom of the chart.
 //Note: You'll need to use python -m http.server to run the visualization. This will host the page at localhost:8000 in your web browser.
+
+//Set up Scatter Plot Chart
+
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 60,
+  left: 50
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create SVG wrapper
+var svg_wrapper = d3
+  .select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var chartGroup = svg_wrapper.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Import data 
+d3.csv("assets/data/data.csv").then(function(CensusData) {
+  CensusData.forEach(function(data) {
+    data.age = +data.age;
+    data.smokes = +data.smokes;
+    // console.log(data);
+  });
+
+  // Scales
+  const xScale = d3.scaleLinear()
+    .domain(d3.extent(CensusData, d => d.age))
+    .range([0, width])
+    .nice(); 
+
+  const yScale = d3.scaleLinear()
+    .domain([6,d3.max(CensusData, d => d.smokes)])
+    .range([height, 0])
+    .nice();
+  
+  // X and Y Axis
+  const xAxis = d3.axisBottom(xScale);
+  const yAxis = d3.axisLeft(yScale);
+
+
+// Chartgroup axes
+  chartGroup.append("g").attr("transform", `translate(0, ${height})`).call(xAxis);
+  chartGroup.append("g").call(yAxis);
+
+//Create Scatter plot
+chartGroup.selectAll("circle")
+.data(CensusData)
+.enter()
+.append("circle")
+.attr("cx", d=>xScale(d.age))
+.attr("cy", d=>yScale(d.smokes))
+.attr("r", "10")
+.attr("stroke-width", "1")
+.classed("stateCircle", true)
+.attr("opacity", 0.90);
+
+//Data Point Text State Abbrevations
+chartGroup.append("g")
+  .selectAll('text')
+  .data(CensusData)
+  .enter()
+  .append("text")
+  .text(d=>d.abbr)
+  .attr("x",d=>xScale(d.age))
+  .attr("y",d=>yScale(d.smokes))
+  .classed(".stateText", true)
+  .attr("font-family", "sans-serif")
+  .attr("text-anchor", "middle")
+  .attr("fill", "white")
+  .attr("font-size", "12px")
+  .style("font-weight", "bold")
+  .attr("alignment-baseline", "central");
+  
+  // Titles for X and Y Axis
+  chartGroup.append("text")
+        .attr("transform", `translate(${width / 2}, ${height + margin.top + 13})`)
+        .attr("text-anchor", "middle")
+        .attr("font-size", "18px")
+        .attr("fill", "black")
+        .text("Median Age");
+
+        chartGroup.append("text")
+        .attr("y", 0 - ((margin.left / 2) + 2))
+        .attr("x", 0 - (height / 2))
+        .attr("text-anchor", "middle")
+        .attr("font-size", "18px")
+        .attr("fill", "black")
+        .attr("transform", "rotate(-90)")
+        .text("Smokers (%)");
+}).catch(function(error) {
+  console.log(error);
+});
